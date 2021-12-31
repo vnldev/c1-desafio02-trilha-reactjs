659273121a8417da23232e9b1730ac0699321856
Victor Lima <victor-lima26@hotmail.com>
2021-12-31 01:41:20 -0300

    finish Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
+import { useWatchMe } from "../hooks/useWatchMe";
+import { MovieCard } from "./MovieCard";
+import { Header } from "./Header";
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useWatchMe();
+
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }

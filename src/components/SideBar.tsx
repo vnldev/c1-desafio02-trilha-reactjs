159ee65674911608bf987c3b78397211f9c2fb71
Victor Lima <victor-lima26@hotmail.com>
2021-12-31 01:40:56 -0300

    finish SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { useWatchMe } from "../hooks/useWatchMe";
+import { Button } from "./Button";
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, changeSelectedGenre, selectedGenre } = useWatchMe();
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => changeSelectedGenre(genre.id)}
+            selected={selectedGenre.id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
